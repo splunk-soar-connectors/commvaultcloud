2db4e1806ce47119a0129ce609f701acf8688a5b
mishalp-crest <mishalp@splunk.com>
2024-05-30 14:37:29 +0530

    minor changes

--- commvaultcloud_consts.py
+++ commvaultcloud_consts.py
@@ -1,3 +1,18 @@
+# File: commvaultcloud_consts.py
+#
+# Copyright (c) Commvault Systems, 2024
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under
+# the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+# either express or implied. See the License for the specific language governing permissions
+# and limitations under the License.
+
 EVENT_ID = "event_id"
 EVENT_TIME = "event_time"
 ANOMALY_SUB_TYPE = "anomaly_sub_type"
